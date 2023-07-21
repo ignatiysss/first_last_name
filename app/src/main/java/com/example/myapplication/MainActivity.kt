f9dce192da1e0beb86d0826f0111dae1d074c080
ignatiys <ignatiysmuller@gmail.com>
2023-07-21 13:18:05 +0100

    recreate form

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonApply)
+        val editText = findViewById<EditText>(R.id.editCountry)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+
+            Toast.makeText(this, "Ти реально думав що мені буде не лінь писати логіку???", Toast.LENGTH_SHORT).show()
+        }
     }
 }
