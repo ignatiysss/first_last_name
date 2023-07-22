47e0a2c52a3baf9a4dd08c08f976eefa9ea93162
ignatiys <ignatiysmuller@gmail.com>
2023-07-22 15:57:37 +0100

    binding update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val button = findViewById<Button>(R.id.buttonApply)
+
         val firstName = findViewById<EditText>(R.id.editFirstName)
         val lastName = findViewById<EditText>(R.id.editLastName)
         val birthDay = findViewById<EditText>(R.id.editBirthDay)
         val country = findViewById<EditText>(R.id.editCountry)
-        button.setOnClickListener {
+        binding.buttonApply.setOnClickListener {
             Toast.makeText(this, "Значить так ти ${firstName.text.toString()} ${lastName.text.toString()}, народився ${birthDay.text.toString()}, в ${country.text.toString()}", Toast.LENGTH_SHORT).show()
         }
-        val buttonPlusOne = findViewById<Button>(R.id.buttonPlusOne)
         var counter = 0
-        buttonPlusOne.setOnClickListener {
+        binding.buttonPlusOne.setOnClickListener {
             counter++
             findViewById<TextView>(R.id.tvCounter).text = "Counter: $counter"
         }
 
-        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
-
-        buttonAdd.setOnClickListener {
+        binding.buttonAdd.setOnClickListener {
             findViewById<TextView>(R.id.tvResult).text = (findViewById<EditText>(R.id.editFirstNumber).text.toString().toInt() +
                     findViewById<EditText>(R.id.editSecondNumber).text.toString().toInt()).toString()
+        }
+        binding.smthId.setOnClickListener {
+            Toast.makeText(this, "Значить так ти ${firstName.text.toString()} ${lastName.text.toString()}, народився ${birthDay.text.toString()}, в ${country.text.toString()}", Toast.LENGTH_SHORT).show()
         }
     }
 }
