6024bfdfc063ef0b7c5350bb8c33109f8d947a18
ignatiys <ignatiysmuller@gmail.com>
2023-07-22 12:02:53 +0100

    add sum

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +26,13 @@ class MainActivity : AppCompatActivity() {
         buttonPlusOne.setOnClickListener {
             counter++
             findViewById<TextView>(R.id.tvCounter).text = "Counter: $counter"
+        }
+
+        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+
+        buttonAdd.setOnClickListener {
+            findViewById<TextView>(R.id.tvResult).text = (findViewById<EditText>(R.id.editFirstNumber).text.toString().toInt() +
+                    findViewById<EditText>(R.id.editSecondNumber).text.toString().toInt()).toString()
         }
     }
 }
