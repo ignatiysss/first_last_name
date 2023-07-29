f3accd57f7e08d0eff73f5c128d402a84c8c6c4c
ignatiys <ignatiysmuller@gmail.com>
2023-07-29 12:15:45 +0000

    add todo list to first fragment (not working)

---
+++ app/src/main/java/com/example/myapplication/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+class Todo(
+    val title: String,
+    val isChecked: Boolean
+)
---
+++ app/src/main/java/com/example/myapplication/TodoAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+    var todos: List<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_todo,
+            parent, false
+        )
+
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.itemView.apply {
+            findViewById<EditText>(R.id.textViewTodo).text =
+                Editable.Factory.getInstance().newEditable(todos[position].title)
+            findViewById<CheckBox>(R.id.checkboxDone).isChecked = todos[position].isChecked
+        }
+    }
+
+    override fun getItemCount(): Int = todos.size
+}
