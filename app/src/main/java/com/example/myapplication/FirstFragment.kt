eda8824b58763a0e69e62facf752c4dc9fb0b5a8
ignatiys <ignatiysmuller@gmail.com>
2023-08-01 19:59:14 +0000

    Revert "add todo list to first fragment (not working)"
    This reverts commit f3accd57f7e08d0eff73f5c128d402a84c8c6c4c.

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -1,12 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.FragmentFirstBinding
 
 /**
@@ -32,24 +31,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFragmentFirstAdd.setOnClickListener {
-            var todoList = mutableListOf(
-                Todo("smth", true)
-            )
-            val adapter = TodoAdapter(todoList)
-
-            binding.recycleViewTodos.adapter = adapter
-            binding.recycleViewTodos.layoutManager = LinearLayoutManager(
-                parentFragment?.context ?: context
-            )
-
-
-            val title = _binding!!.editNewTodos.text.toString()
-            val todo = Todo(title, false)
-            todoList.add(todo)
-            adapter.notifyItemInserted(todoList.size - 1)
-        }
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
--- app/src/main/java/com/example/myapplication/Todo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication
-
-class Todo(
-    val title: String,
-    val isChecked: Boolean
-)
